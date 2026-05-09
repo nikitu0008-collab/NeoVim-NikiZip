991d7dc5b95c3e5c5e90f5af1c83f40071de5ded
NikiZip <nikitu0008@gmail.com>
2026-05-09 12:33:45 +0300

    v1.0.1
    Add:
        1.Double enter line break where the cursor is.
        2.Double backspace deletes the character to the left of the cursor.
        3.Increased travel speed j, k.

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,6 +27,32 @@ vim.opt.winbar = "%{%v:lua.custom_winbar()%}"
 
 vim.opt.rtp:prepend(lazypath)
 
+vim.env.PATH = vim.env.PATH .. ':' .. vim.fn.expand('~/.dotnet/tools')
+
+-- Настройка таймаута для различения одиночного и двойного нажатия (опционально)
+vim.o.timeout = true          -- Включить таймаут для последовательностей клавиш
+vim.o.timeoutlen = 500        -- Максимальное время между нажатиями (мс)
+
+-- 1. Двойной Enter: разбить строку в позиции курсора (перенос остатка на новую строку)
+vim.keymap.set('n', '<CR><CR>', 'i<CR><Esc>', { desc = 'Split line at cursor' })
+
+-- 2. Двойной Backspace: удалить символ слева от курсора (как Backspace в режиме вставки)
+vim.keymap.set('n', '<BS><BS>', 'X', { desc = 'Delete character before cursor' })
+
+-- optimization moved j, k 
+
+-- Ускорение отрисовки (меньше лагов при быстрой прокрутке)
+vim.opt.lazyredraw = true
+
+-- Уменьшить время ожидания key-mapped sequences
+vim.opt.timeoutlen = 300
+
+-- Синхронизация синтаксиса с minlines для больших файлов
+vim.opt.synmaxcol = 128
+
+-- Оставить курсор по центру экрана при прокрутке
+vim.opt.scrolloff = 8
+
 require("options")
 require("keymaps")
 
